4e48abfa5fea31b36752e404964caef38499524c
bageerathan <bageerathan@rcktechiees.com>
2019-10-14 13:35:11 +0530

    updates to deploy to heroku

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/test'));
+app.use(express.static(__dirname + '/dist/testcmp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/test/index.html'));
+res.sendFile(path.join(__dirname+'/dist/testcmp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
